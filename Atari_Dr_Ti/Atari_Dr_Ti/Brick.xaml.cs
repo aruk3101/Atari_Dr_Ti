27354eaf92629f4961362335b5daff97ff9d81ca
tomasz.iwanski <tomekV0oid123@gmail.com>
2024-03-01 10:23:17 +0100

    Ball and Brick Created

---
+++ Atari_Dr_Ti/Atari_Dr_Ti/Brick.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Atari_Dr_Ti
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Brick : ContentView
+    {
+        public Brick()
+        {
+            InitializeComponent();
+        }
+    }
+}
