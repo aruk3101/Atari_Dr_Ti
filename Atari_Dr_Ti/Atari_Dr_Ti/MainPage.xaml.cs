1570065a3e2e2d970650078947528c7cff66aee4
damian.raczek <draczekprojekt@gmail.com>
2024-03-01 10:35:19 +0100

    Ball move & interaction with bricks, player and walls implemented.

--- Atari_Dr_Ti/Atari_Dr_Ti/MainPage.xaml.cs
+++ Atari_Dr_Ti/Atari_Dr_Ti/MainPage.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Atari_Dr_Ti
@@ -12,7 +9,7 @@ namespace Atari_Dr_Ti
     public partial class MainPage : ContentPage
     {
         List<Action> actions = new List<Action>();
-        //List<Brick> bricks = new List<Brick>();
+        List<Brick> bricks = new List<Brick>();
         double screenWidth;
         double screenHeight;
 
@@ -32,7 +29,56 @@ namespace Atari_Dr_Ti
         private void MainPage_SizeChanged(object sender, EventArgs e)
         {
             this.screenHeight = Application.Current.MainPage.Height;
-            this.screenHeight = Application.Current.MainPage.Height;
+            this.screenWidth = Application.Current.MainPage.Width;
+            GenerateBricks();
+        }
+
+        private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            if (playerBounds != null && playerBounds.Y != 0)
+            {
+                float accX = -e.Reading.Acceleration.X;
+                Console.WriteLine(accX);
+                if (accX < 0)
+                {
+                    if (playerBounds.X < 5)
+                        accX = -accX;
+                    playerBounds.X += Math.Floor(accX) * 3;
+                }
+                else
+                {
+                    if (playerBounds.X + playerBounds.Width > screenWidth - 5) accX = -accX;
+                    playerBounds.X += Math.Ceiling(accX) * 3;
+                }
+                AbsoluteLayout.SetLayoutBounds(player, playerBounds);
+            }
+            Console.WriteLine(playerBounds.X);
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                foreach (var action in actions)
+                {
+                    action.Invoke();
+                }
+            });
+        }
+
+        private void GenerateBricks()
+        {
+            int no = (int)(screenWidth / 50);
+            for (int i = (int)(screenWidth - (no * 50)) / 2; i <= screenWidth - 50; i += 50)
+            {
+                for (int j = 0; j < 60; j += 20)
+                {
+                    Brick b = new Brick();
+                    layout.Children.Add(b);
+                    AbsoluteLayout.SetLayoutBounds(b, new Rectangle(i, j, 50, 20));
+                    bricks.Add(b);
+                }
+            }
         }
 
         protected override void OnAppearing()
@@ -40,13 +86,64 @@ namespace Atari_Dr_Ti
             base.OnAppearing();
             Timer timer = new Timer();
             timer.Interval = 5;
-            timer.Enabled = true;
+            timer.Elapsed += Timer_Elapsed;
             timer.Start();
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+            Accelerometer.Start(SensorSpeed.UI);
 
+            actions.Add(() =>
+            {
+                currentBallBounds = AbsoluteLayout.GetLayoutBounds(ball);
+                playerBounds = AbsoluteLayout.GetLayoutBounds(player);
+            });
+
+            Action ballMoving = new Action(() =>
+            {
+                currentBallBounds.X += stepx;
+                currentBallBounds.Y += stepy;
+                AbsoluteLayout.SetLayoutBounds(ball, currentBallBounds);
+            });
+            actions.Add(ballMoving);
+
+            actions.Add(() =>
+            {
+                List<Brick> toRemove = new List<Brick>();
+                for (int i = 0; i < bricks.Count; i++)
+                {
+                    Brick b = bricks[i];
+                    if (AbsoluteLayout.GetLayoutBounds(b).IntersectsWith(currentBallBounds))
+                    {
+                        stepy = -stepy;
+                        toRemove.Add(b);
+                        break;
+                    }
+                }
+                foreach (Brick b in toRemove)
+                {
+                    layout.Children.Remove(b);
+                    bricks.Remove(b);
+                }
+
+            });
+
+            int padding = 5;
+            actions.Add(new Action(() =>
+            {
+                if (currentBallBounds.X + padding < 0
+                        || currentBallBounds.X + objectWidth + padding > screenWidth) stepx = -stepx;
+                if (currentBallBounds.Y + padding < 0
+                        || currentBallBounds.Y + padding + objectHeight > screenHeight) stepy = -stepy;
+            }));
+
+            actions.Add(new Action(() =>
+            {
+                Rectangle playerBounds = AbsoluteLayout.GetLayoutBounds(player);
+                if (currentBallBounds.IntersectsWith(playerBounds))
+                {
+                    stepy = -stepy;
+                }
+            }));
         }
-
-
-
-    }
+        }
 }
 
